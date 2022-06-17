f935f2d1fcad758f1959492ed754692befb7665c
Mehmet Celik <mehmet@celik.be>
2022-06-17 14:04:19 +0200

    new mapping for key/value auto complete fields

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,13 +5,11 @@ require_relative 'generic_controller'
 class MainController < GenericController
   get '/' do
     content_type :json
-    result = {
-      "data": data_api?,
-      "audit": audit_api?,
-      "search": search_api?,
-      "browse": browse_api?,
-      "logic": logic_api?
-    }
+    result = {}
+    ConfigFile[:services].each_key do |k|
+      next if k.eql?(:ping)
+      result[k] = service_alive?(k)
+    end
 
     status 500 if result.flatten.select{|s| s.is_a?(Hash)}.map{|m| m.values}.flatten.include?(false)
 
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -4,56 +4,32 @@ require 'lib/config_file'
 module Sinatra
   module MainHelper
     def endpoints
-      settings.solis.list_shapes.map {|m| "/#{m.tableize}"}.sort
+      settings.solis.list_shapes.map { |m| "/#{m.tableize}" }.sort
     end
 
-    def api_error(status, source, title="Unknown error", detail="")
+    def api_error(status, source, title = "Unknown error", detail = "")
       content_type :json
-      {"errors": [{
-                    "status": status,
-                    "source": {"pointer":  source},
-                    "title": title,
-                    "detail": detail
-                  }]}
+      { "errors": [{
+                     "status": status,
+                     "source": { "pointer": source },
+                     "title": title,
+                     "detail": detail
+                   }] }
     end
 
-    def data_api?
-      result = HTTP.get("#{ConfigFile[:services][:data][:host]}/#{ConfigFile[:services][:data][:base_path]}")
+    def service_alive?(k)
+      alive = {}
+      result = HTTP.timeout(3).get("#{ConfigFile[:services][k][:host]}#{ConfigFile[:services][k][:base_path]}")
       api = result.body.to_s.length > 0 ? true : false
-      result = HTTP.post("#{ConfigFile[:services][:data][:host]}/_sparql", form: {query: 'ask where{?s ?p ?o}'})
+      alive['api'] = api if result.status == 200
+
+      result = HTTP.post("#{ConfigFile[:services][k][:host]}/_sparql", form: { query: 'ask where{?s ?p ?o}' })
       storage = result.body.to_s =~ /TRUE \.$/ ? true : false
+      alive['storage'] = storage if result.status == 200
 
-      return {'api': api, 'storage': storage}#
+      alive
     rescue HTTP::Error => e
-      return {'api': false, 'storage': false}
-    end
-
-    def audit_api?
-      result = HTTP.get("#{ConfigFile[:services][:audit][:host]}/#{ConfigFile[:services][:audit][:base_path]}/ping")
-      return ::JSON.parse(result.body.to_s)
-    rescue HTTP::Error => e
-      return {'api': false, 'storage': false}
-    end
-
-    def search_api?
-      result = HTTP.get("#{ConfigFile[:services][:search][:host]}/#{ConfigFile[:services][:search][:base_path]}/ping")
-      return ::JSON.parse(result.body.to_s)
-    rescue HTTP::Error => e
-      return {'api': false, 'storage': false}
-    end
-
-    def browse_api?
-      result = HTTP.get("#{ConfigFile[:services][:browse][:host]}/#{ConfigFile[:services][:browse][:base_path]}/ping")
-      return ::JSON.parse(result.body.to_s)
-    rescue HTTP::Error => e
-      return {'api': false, 'storage': false}
-    end
-
-    def logic_api?
-      result = HTTP.get("#{ConfigFile[:services][:logic][:host]}/#{ConfigFile[:services][:logic][:base_path]}/ping")
-      return ::JSON.parse(result.body.to_s)
-    rescue HTTP::Error => e
-      return {'api': false, 'storage': false}
+      { 'api': false }
     end
 
   end
